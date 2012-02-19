b8e3bf81e2b51e100fd0b0f449894934add90e46
tacker <adgjmptwxx@gmail.com>
2012-02-20 02:22:07 +0900

    不要物削除

--- trunk/etc/batch/makedb.bat
+++
@@ -1,5 +0,0 @@
-SET MYSQL=mysql
-SET USER=root
-SET PASSWORD=
-%MYSQL%  --user=%USER% --password=%PASSWORD% --default-character-set=utf8 < ..\..\db\makedb.sql
-%MYSQL%  --user=%USER% --password=%PASSWORD% --default-character-set=utf8 < ..\..\db\make_tables.sql

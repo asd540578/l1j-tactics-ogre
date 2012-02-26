046c4eb44cbec9dccfbf0a70d20f8e72ca3b25b1
tacker <adgjmptwxx@gmail.com>
2012-02-27 02:59:26 +0900

    l1j-tw-99nets r337適用

--- trunk/src/l1j/server/server/clientpackets/C_DropItem.java
+++ trunk/src/l1j/server/server/clientpackets/C_DropItem.java
@@ -22,6 +22,7 @@ package l1j.server.server.clientpackets;
 import java.util.logging.Logger;
 
 import l1j.server.server.ClientThread;
+import l1j.server.server.model.L1ItemCheck;
 import l1j.server.server.model.L1World;
 import l1j.server.server.model.Instance.L1DollInstance;
 import l1j.server.server.model.Instance.L1ItemInstance;
@@ -40,6 +41,10 @@ public class C_DropItem extends ClientBasePacket {
 		int objectId = readD();
 		int count = readD();
 
+		if (count > 0x77359400 || count < 0) { // 確保數量不會溢位
+			count = 0;
+		}
+
 		L1PcInstance pc = client.getActiveChar();
 		if (pc.isGhost()) {
 			return;
@@ -50,6 +55,10 @@ public class C_DropItem extends ClientBasePacket {
 
 		L1ItemInstance item = pc.getInventory().getItem(objectId);
 		if (item != null) {
+			L1ItemCheck checkItem = new L1ItemCheck(); // 物品狀態檢查
+			if (checkItem.ItemCheck(item, pc)) { // 是否作弊
+				return;
+			}
 			if (!item.getItem().isTradable()) {
 				// \f1%0は捨てたりまたは他人に讓ることができません。
 				pc.sendPackets(new S_ServerMessage(210, item.getItem()
